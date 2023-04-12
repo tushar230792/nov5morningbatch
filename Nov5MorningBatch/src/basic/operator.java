80aa98e9aeb08c7df78312a60cf9769e33f5514e
tushar <tushar@DESKTOP-F3KEME9>
2023-04-12 11:42:07 +0530

    tesr case 5 nov change

---
+++ Nov5MorningBatch/src/basic/operator.java
@@ -0,0 +1,37 @@
+package basic;
+
+public class operator {
+	
+	public class Operator {
+		
+		public static void main(String[] args) {
+			
+			// variable declaration and initilization 
+			
+			int a=20, b=10;
+			
+			//addional operator
+			System.out.println("a+b=" + (a+b));
+			
+			//subtraction oerator
+			System.out.println("a-b=" + (a-b));
+			
+			//multiplication operator
+			System.out.println("a*b=" + (a*b));
+			
+			//division operator
+			System.out.println("a/b=" + (a/b));  //unkniwn extra
+			
+				
+			
+			//division operator
+			System.out.println("a/b=" + (a/b));
+			
+			//module operator
+			System.out.println("a%b=" + (a%b));
+			
+		}
+
+		}
+
+}
